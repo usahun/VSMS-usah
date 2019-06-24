4a2839e6ad66d7bc7d3e417e127c4c08ac9735de
usahun <khmeruser2@gmail.com>
2019-06-24 17:27:28 +0700

    [add-26-06-2019]

--- VSMS-Develope-Developer/VSMS/Controller/MenuViewController.swift
+++ VSMS-Develope-Developer/VSMS/Controller/MenuViewController.swift
@@ -2,62 +2,29 @@
 //  MenuViewController.swift
 //  VSMS
 //
-//  Created by usah on 6/4/19.
+//  Created by Vuthy Tep on 6/24/19.
 //  Copyright © 2019 121. All rights reserved.
 //
 
 import UIKit
 
-class SectionMenu: UITableViewCell{
-    
-    @IBOutlet weak var content: UILabel!
-    
-}
-
-class MenuViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
-  
+class MenuViewController: UIViewController {
    
     
-    @IBOutlet weak var tableView: UITableView!
     
-    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var imageprofile: UIImageView!
+    
+    let titleArray = ["Your Profile","Your Posts","Your Like",
+                     "Your Share","Setting","About Us","Term of Privancy"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageprofile.layer.cornerRadius = imageprofile.frame.width * 0.5
+        imageprofile.clipsToBounds = true
         
-        img.layer.cornerRadius = img.frame.width * 0.5
-        img.clipsToBounds = true
         
-        
-
-        // Do any additional setup after loading the view.
     }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 7
-    }
-    
-  
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SectionMenu
-        
-        let row = indexPath.row
-        if row == 0 {
-            cell.content.text? = "Your  Profile"
-        }else if row == 1{
-            cell.content.text? = "Your  Posts"
-        }else if row == 2{
-            cell.content.text? = "Your  Like"
-        }else if row == 3{
-            cell.content.text? = "Your  Loan"
-        }else if row == 4{
-            cell.content.text? = "Sing Out"
-        }else if row == 5{
-            cell.content.text? = "About Us"
-        }else if row == 6{
-            cell.content.text? = "Term of Privacy"
-        }
-        return cell
-    }
+   
 
 }
 
