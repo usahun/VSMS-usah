324e0f7d11f51203fc6fe9c1862595f0b336f535
usahun <khmeruser2@gmail.com>
2019-07-09 09:26:42 +0700

    commit again

---
+++ VSMS-Develope-Developer/VSMS/Model/Protocol.swift
@@ -0,0 +1,43 @@
+//
+//  Protocol.swift
+//  VSMS
+//
+//  Created by Vuthy Tep on 7/3/19.
+//  Copyright © 2019 121. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SwiftyJSON
+
+protocol XibtoXib {
+    func getDropDownID(Id: String)
+}
+
+protocol refreshDropdownInXib: class {
+    func refreshXib(FKKey: Int)
+}
+
+class CellClickViewModel {
+    var ID: String = ""
+    var Value: String = ""
+    var IndexPathKey: NSIndexPath? = nil
+}
+
+struct dropdownData {
+    var ID: String
+    var Text: String
+    var FKKey: String?
+    
+    init(json: JSON) {
+        self.ID = json["id"].stringValue
+        self.Text = json["Text"].stringValue
+        self.FKKey = json["FKKey"].string
+    }
+    
+    init(ID: String, Text: String, FKKey: String? ) {
+        self.ID = ID
+        self.Text = Text
+        self.FKKey = FKKey
+    }
+}
